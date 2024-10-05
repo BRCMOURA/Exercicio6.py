dad06086cd7dfcfde52647f3b5a0ddefb0fab5ea
BRCMOURA <brcmoura@gmail.com>
2024-10-04 21:10:17 -0300

    Add files via upload

---
+++ Exercício6.py
@@ -0,0 +1,8 @@
+#Potência de um número
+
+n1 = int(input("Digite um número: "))
+expoente = int(input("Digite o expoente: "))
+
+numero = (n1) ** (expoente)
+
+print("O número elevado a esse expoente é: ", numero)
